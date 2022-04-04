36a731f9316084eded9e83e405cc27891f896ba1
Priya <agrpriya.2001@gmail.com>
2022-04-04 19:24:54 +0530

    navbar component

---
+++ src/Components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        name: 'Home',
+        url: '#',
+        cname: 'nav-links'
+    },
+    {
+        name: 'Discuss',
+        url: '#',
+        cname: 'nav-links'
+    },
+    {
+        name: 'Contact Us',
+        url: '#',
+        cname: 'nav-links'
+    },
+    {
+        name: 'Sign Up',
+        url: '#',
+        cname: 'btn'
+    }
+]
